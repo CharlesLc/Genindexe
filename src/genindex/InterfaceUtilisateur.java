c9c316607ed8b58929338403fbdc642399997033
Maëva <Mava@172.16.124.184>
2015-05-19 09:44:27 +0200

    Ajout du validateur

--- src/genindex/InterfaceUtilisateur.java
+++ src/genindex/InterfaceUtilisateur.java
@@ -4,6 +4,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.sql.*;
 
 /**
  * 18/05/2015
@@ -11,9 +12,12 @@ import javax.swing.*;
  */
 public class InterfaceUtilisateur  extends JPanel{
    private JButton bouNouvClient, bouNouvCommande, bouNouvEspece, bouNouvCategorie;
+   private JButton bouVisuCommande, bouNouvScrapie, bouNouvSexing;
    private JLabel labFonctionUser, labNomUser, labPrenomUser;
    private JTextArea textFonctionUser, textNomUser, textPrenomUser;
    private Frame_mother frame;
+   private JPasswordField pass1;
+   private JTextField tf1;
    
    public InterfaceUtilisateur (Frame_mother formulaireClient)
    {
@@ -36,6 +40,10 @@ public class InterfaceUtilisateur  extends JPanel{
            bouNouvEspece = new JButton ("Créer Nouvelle Espece");
            bouNouvCategorie = new JButton ("Créer Nouvelle Catégorie Espece");
            
+           bouVisuCommande = new JButton ("Visualiser la Commande");
+           bouNouvScrapie = new JButton ("Créer Test de Tremblement");
+           bouNouvSexing = new JButton ("Créer Test de Sexage");
+           
            //Labels
            labFonctionUser = new JLabel ("Fonction: ");
            labFonctionUser.setHorizontalAlignment(JLabel.CENTER);
@@ -48,6 +56,9 @@ public class InterfaceUtilisateur  extends JPanel{
            labPrenomUser = new JLabel ("Prénom: ");
            labPrenomUser.setHorizontalAlignment(JLabel.CENTER);
            labPrenomUser.setVerticalAlignment(JLabel.CENTER);
+           
+           tf1 = new JTextField();
+           pass1 = new JPasswordField();
            
            //TextArea
            textFonctionUser = new JTextArea(3, 20);
@@ -77,8 +88,44 @@ public class InterfaceUtilisateur  extends JPanel{
            panelSecretaire.add(bouNouvCommande);
            panelSecretaire.add(bouNouvEspece);
            panelSecretaire.add(bouNouvCategorie);
-           panelSecretaire.isVisible();
            
+           //Troisieme panel: les actions que peut faire le validateur
+           //ce panel sera mis en visible(false) si l'utilisateur est une secrétaire
+           //ou un technicien
+           JPanel panelTechnicien = new JPanel();
+           panelTechnicien.setLayout(new GridLayout(3,1));
+           panelTechnicien.add(bouVisuCommande);
+           panelTechnicien.add(bouNouvScrapie);
+           panelTechnicien.add(bouNouvSexing);
+           
+           panelSecretaire.setVisible(true);
+           panelTechnicien.setVisible(false);
+           
+           
+           String str1 = tf1.getText();
+        char[] p = pass1.getPassword();
+        String str2 = new String(p);
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.24.16/td2","td2","OST");
+            PreparedStatement ps = con.prepareStatement("select jobid from user where login=? and passwd=?");
+            ps.setString(1, str1);
+            ps.setString(2, str2);
+            ResultSet rs = ps.executeQuery();
+            //le jobid de Secretaire est 1
+            if (rs.isFirst()){
+                panelSecretaire.setVisible(true);
+            //le jobid de Technicien est 3
+            }else if (rs.isLast()){
+                panelTechnicien.setVisible(true);
+            }
+             //faire un else avec le validateur (son jobid est 2)           
+            }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
            
            
            //Panel regroupant les autres panels
@@ -86,13 +133,17 @@ public class InterfaceUtilisateur  extends JPanel{
            panelGeneral.setLayout(new GridLayout(2,1));
            panelGeneral.add(infoUser);
            panelGeneral.add(panelSecretaire);           
+           panelGeneral.add(panelTechnicien);
            
            //this setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
            this.add(panelGeneral);
            //this.pack();
-           this.setVisible(true);          
+           this.setVisible(true); 
+           
    }
    
+   
+    
    public JPanel getPanel(){
         return this;
     } 
